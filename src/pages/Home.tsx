cf863bcf2077cca9e16774f050fe6ba336d83ff8
Felipe <felipescherer25@gmail.com>
2021-06-24 10:32:53 -0300

    Login into rooms and started styling room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,40 @@ import { useHistory } from 'react-router'
 
 
 
-import { auth, firebase } from '../services/firebase'
+import { auth, database, firebase } from '../services/firebase'
 import '../styles/auth.scss'
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent } from 'react'
+import { useState } from 'react'
 
 export function Home(){
   const history = useHistory();
   const { signInWithGoogle, user } = useAuth();
-  
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if(!user) {
       await signInWithGoogle()
     }
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event : FormEvent){
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exist!')
+      return;
+    }
+
+    history.push(`rooms/${roomCode}`)
+
   }
 
  return (
@@ -37,10 +57,12 @@ export function Home(){
         Crie sua sala com o Google
       </button>
       <div className="separator">ou entre em uma sala</div>
-      <form>
+      <form onSubmit={handleJoinRoom}>
           <input 
             type="text"
             placeholder="Digite o código da sala"
+            onChange={event => setRoomCode(event.target.value)}
+            value={roomCode}
           />
           <Button type="submit">
             Entrar na sala
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,36 @@
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button'
+
+import '../styles/rooms.scss';
+
 export function Room(){
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="class-name-content">
+          <img src={logoImg} alt="Logo" />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea
+            placeholder="O que você deseja perguntar?"
+          />
+          
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+
+    </div>
   )
 }
